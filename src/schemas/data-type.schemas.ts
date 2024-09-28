a8f61fa21f8418c316df6479915ccaadf96036ea
Ravindu Ponnamperuma <90395607+RavinduPonnamperuma@users.noreply.github.com>
2024-09-28 22:27:43 +0530

    add users crud

--- src/schemas/data-type.schemas.ts
+++
@@ -1,9 +0,0 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-
-@Entity("tbl_data_type")
-export class DataSource extends BaseEntity {
-  @PrimaryGeneratedColumn({ name: "data_type_id" })
-  dataTypeId: number;
-  @Column({ name: "data_type_name" })
-  dataTypeName: string;
-}
