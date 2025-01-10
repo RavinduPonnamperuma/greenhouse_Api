ae01c6a2dfd4affba60054ea424c2a81d255470d
Ravindu Ponnamperuma <90395607+RavinduPonnamperuma@users.noreply.github.com>
2025-01-10 17:52:40 +0530

    add admin nav

--- src/modules/mqtt/mqtt.service.ts
+++ src/modules/mqtt/mqtt.service.ts
@@ -1,18 +1,78 @@
-import { Injectable, OnModuleInit, Logger } from '@nestjs/common'
-import * as mqtt from 'mqtt'
+// import { Injectable, OnModuleInit, Logger } from '@nestjs/common'
+// import * as mqtt from 'mqtt'
+//
+// @Injectable()
+// export class MqttService implements OnModuleInit {
+//   private client: mqtt.MqttClient
+//   private readonly logger = new Logger(MqttService.name)
+//
+//   constructor() {
+//     this.logger.log('MqttService instantiated')
+//   }
+//
+//   onModuleInit() {
+//     this.logger.log('Initializing module and connecting to broker')
+//     this.connectToBroker()
+//   }
+//
+//   private connectToBroker() {
+//     const options: mqtt.IClientOptions = {
+//       host: '15.235.192.41',
+//       port: 1883,
+//       username: 'sadee',
+//       password: 'qwerty',
+//     }
+//
+//     this.client = mqtt.connect(options)
+//     this.logger.log(`Attempting to connect to MQTT broker at ${options.host}:${options.port}`)
+//
+//     const topics = ['esp/output1', 'esp/1/adxl345', 'esp/sensor', 'esp/1/accident']
+//
+//     this.client.on('connect', () => {
+//       this.logger.log('Successfully connected to MQTT broker')
+//       this.client.subscribe('esp/output1', {}, (error, granted) => {
+//         if (error) {
+//           this.logger.error('Subscription error:', error)
+//         } else {
+//           granted.forEach(grant => {
+//             this.logger.log(`Successfully subscribed to topic ${grant.topic} with QoS ${grant.qos}`)
+//           })
+//         }
+//       })
+//     })
+//
+//     this.client.on('message', (topic, message) => {
+//       this.handleMessage('esp/output1', message.toString())
+//     })
+//
+//   }
+//
+//   private handleMessage(topic: string, message: string) {
+//     const sensorData = JSON.parse(message)
+//      console.log(sensorData,topic)
+//   }
+// }
+import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
+import * as mqtt from 'mqtt';
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
 @Injectable()
+@WebSocketGateway({ cors: true }) // Enable CORS for WebSocket
 export class MqttService implements OnModuleInit {
-  private client: mqtt.MqttClient
-  private readonly logger = new Logger(MqttService.name)
+  private client: mqtt.MqttClient;
+  private readonly logger = new Logger(MqttService.name);
+
+  @WebSocketServer()
+  private server: Server; // WebSocket server
 
   constructor() {
-    this.logger.log('MqttService instantiated')
+    this.logger.log('MqttService instantiated');
   }
 
   onModuleInit() {
-    this.logger.log('Initializing module and connecting to broker')
-    this.connectToBroker()
+    this.logger.log('Initializing module and connecting to broker');
+    this.connectToBroker();
   }
 
   private connectToBroker() {
@@ -21,44 +81,41 @@ export class MqttService implements OnModuleInit {
       port: 1883,
       username: 'sadee',
       password: 'qwerty',
-    }
+    };
 
-    this.client = mqtt.connect(options)
-    this.logger.log(`Attempting to connect to MQTT broker at ${options.host}:${options.port}`)
-
-    const topics = ['esp/output1', 'esp/1/adxl345', 'esp/sensor', 'esp/1/accident']
+    this.client = mqtt.connect(options);
+    this.logger.log(`Attempting to connect to MQTT broker at ${options.host}:${options.port}`);
 
     this.client.on('connect', () => {
-      this.logger.log('Successfully connected to MQTT broker')
+      this.logger.log('Successfully connected to MQTT broker');
       this.client.subscribe('esp/output1', {}, (error, granted) => {
         if (error) {
-          this.logger.error('Subscription error:', error)
+          this.logger.error('Subscription error:', error);
         } else {
-          granted.forEach(grant => {
-            this.logger.log(`Successfully subscribed to topic ${grant.topic} with QoS ${grant.qos}`)
-          })
+          granted.forEach((grant) => {
+            this.logger.log(`Successfully subscribed to topic ${grant.topic} with QoS ${grant.qos}`);
+          });
         }
-      })
-    })
+      });
+    });
 
     this.client.on('message', (topic, message) => {
-      this.handleMessage('esp/output1', message.toString())
-    })
-    // this.client.on('message', (topic, message) => {
-    //   this.handleMessage('esp/1/adxl345', message.toString())
-    // })
-    // this.client.on('message', (topic, message) => {
-    //   this.handleMessage('esp/1/accident', message.toString())
-    // })
-    //
-    // this.client.on('error', (error) => {
-    //   this.logger.error('MQTT connection error:', error)
-    // })
+      this.handleMessage(topic, message.toString());
+    });
   }
 
   private handleMessage(topic: string, message: string) {
-    const sensorData = JSON.parse(message)
-    console.log(sensorData,topic
-    )
+    try {
+      const sensorData = JSON.parse(message);
+      this.logger.log(`Received message on topic ${topic}: ${JSON.stringify(sensorData)}`);
+      this.emitSensorData(topic, sensorData); // Emit data to WebSocket
+    } catch (error) {
+      this.logger.error('Error parsing MQTT message:', error);
+    }
+  }
+
+  private emitSensorData(topic: string, sensorData: any) {
+    this.server.emit('sensorData', { topic, sensorData });
+    this.logger.log('Sensor data emitted to WebSocket');
   }
 }
