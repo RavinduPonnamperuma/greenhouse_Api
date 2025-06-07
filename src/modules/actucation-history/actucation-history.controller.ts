7deb292677bd146060cb8fc418631fa586dcd300
Thimira Sadeesha <thimira@maximimpressions.com>
2025-06-07 21:04:59 +0530

    style: format imports consistently across multiple files

--- src/modules/actucation-history/actucation-history.controller.ts
+++
@@ -1,7 +0,0 @@
-import {Controller} from '@nestjs/common';
-import {ActucationHistoryService} from './actucation-history.service';
-
-@Controller('actucation-history')
-export class ActucationHistoryController {
-  constructor(private readonly actucationHistoryService: ActucationHistoryService) {}
-}
--- src/modules/actucation-history/actucation-history.module.ts
+++
@@ -1,9 +0,0 @@
-import {Module} from '@nestjs/common';
-import {ActucationHistoryService} from './actucation-history.service';
-import {ActucationHistoryController} from './actucation-history.controller';
-
-@Module({
-  controllers: [ActucationHistoryController],
-  providers: [ActucationHistoryService],
-})
-export class ActucationHistoryModule {}
